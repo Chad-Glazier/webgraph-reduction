22d8bd3f0ad8bdd2cf8ae42b33a2fb756f68c1b2
Chad-Glazier <chadglazier@outlook.com>
2024-11-22 00:18:29 -0800

    avacados are a scam

--- src/main/java/NodeDomainMap.java
+++ src/main/java/NodeDomainMap.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Hashtable;
 
 public class NodeDomainMap {
     private Hashtable<Integer, String> map;
     
-    NodeDomainMap() {
+    NodeDomainMap(Iterable<Integer> nodeIds, String nodeDomainFile) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(nodeDomainFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // process the line.
+            }
+            reader.close();
+        }
+        
+        for (int nodeId : nodeIds) {
 
+        }
     }
 }
